f72211f3d8cfec60c51916b029a89474b8e4f185
ismarads <ismarpp.santos@gmail.com>
2020-06-02 19:58:40 -0300

    hoje criamos o arquivo create-point.html bem como os arquivos js e css que levam o mesmo nome. Refatoramos parte de nosso códiogo css e consumimos Api de estados atraveś do JS.

---
+++ scripts/create-point.js
@@ -0,0 +1,57 @@
+function populateUFs(){
+
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+
+    .then(res =>  res.json())// como se trata de apenas uma resposta a função anonima foi reduzida ao maximo, bem como a solicitação de retorno do json
+    .then(states => {
+
+
+        for(const state of states){
+
+            ufSelect.innerHTML +=  `<option value = "${state.id}"> ${state.nome}</option>`
+        }
+
+       
+
+    })
+
+}
+
+populateUFs()
+
+function getCities(event){
+    const citySelect = document.querySelector("[name=city]")
+    
+    
+    const ufValue = event.target.value
+
+
+  
+
+   
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+
+    .then(res =>  res.json())// como se trata de apenas uma resposta a função anonima foi reduzida ao maximo, bem como a solicitação de retorno do json
+    .then(cities => {
+        for(const city of cities){
+
+            citySelect.innerHTML +=  `<option value = "${city.id}"> ${city.nome}</option>`
+        }
+
+       citySelect.disabled = false
+
+    })
+
+
+}
+
+
+
+
+document
+.querySelector("select[name=uf]")
+.addEventListener( "change", getCities)
